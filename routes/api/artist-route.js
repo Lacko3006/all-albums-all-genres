af07ae5952cad50d23a43772005d2ff7f13f1622
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-01-09 17:46:35 +0000

    created a routes file and connection file to database

---
+++ routes/api/artist-route.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+// create model for artist
+
+// router.get("/", async (req, res) => {
+//     try {
+//         const artistGet = await 
+//     }
+// }
+// )
