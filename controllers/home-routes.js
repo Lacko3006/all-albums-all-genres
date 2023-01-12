b0ca677d4628eb979aad05fc5c7b15022d45696d
timo9939 <timothylau20199@gmail.com>
2023-01-12 21:34:32 +0000

    Connect the Artist page to the database

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,28 +11,15 @@ router.get('/login', async (req, res) => {
 });
 
 
-router.get('/artist', async (req, res) => {
-    res.render('artist')
-
-
-    // try {
-    //   const dbArtistData = await Artist.findByPk(req.params.id, {
-    //     include: [
-    //       {
-    //         model: Artist,
-    //         attributes: [
-    //           'artist_name',
-    //         ],
-    //       },
-    //     ],
-    //   });
-  
-    //   const Artist = dbArtistData.get({ plain: true });
-    //   res.render('artist', { Artist });
-    // } catch (err) {
-    //   console.log(err);
-    //   res.status(500).json(err);
-    // }
+router.get('/artist/:id', async (req, res) => {
+    try {
+      const dbArtistData = await Artist.findByPk(req.params.id);
+      const artistData = dbArtistData.get({ plain: true });
+      res.render('artist', { artistData });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   });
   
   
