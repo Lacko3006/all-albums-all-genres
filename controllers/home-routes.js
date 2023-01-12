e45e8a25e8a446bea227bfe6f4a826a6e340e318
timo9939 <timothylau20199@gmail.com>
2023-01-12 21:09:23 +0000

    Origin Page of Artist

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,5 @@
+const Artist = require("../models/Artist");
+
 const router = require("express").Router();
 
 router.get('/', async (req, res) => {
@@ -8,5 +10,31 @@ router.get('/login', async (req, res) => {
     res.render('login') 
 });
 
+
+router.get('/artist', async (req, res) => {
+    res.render('artist')
+
+
+    // try {
+    //   const dbArtistData = await Artist.findByPk(req.params.id, {
+    //     include: [
+    //       {
+    //         model: Artist,
+    //         attributes: [
+    //           'artist_name',
+    //         ],
+    //       },
+    //     ],
+    //   });
+  
+    //   const Artist = dbArtistData.get({ plain: true });
+    //   res.render('artist', { Artist });
+    // } catch (err) {
+    //   console.log(err);
+    //   res.status(500).json(err);
+    // }
+  });
+  
+  
 
 module.exports = router;
