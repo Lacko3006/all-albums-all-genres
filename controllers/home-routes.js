a8b9977017e61f0bde0514af684c810a18542e27
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-01-16 18:48:35 +0000

    added artist page and link with handlebars for each loop

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,27 +1,52 @@
 const Artist = require("../models/Artist");
-
+const Album = require("../models/Album");
 const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-    res.render('homepage') 
+router.get("/", async (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/login', async (req, res) => {
-    res.render('login') 
+router.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+router.get("/artist", async (req, res) => {
+  try {
+    const dbArtistData = await Artist.findAll({ raw: true });
+    res.render("artist", {
+      dbArtistData,
+    });
+    console.log(dbArtistData)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET all albums for album page
+router.get("/album", async (req, res) => {
+  try {
+    const dbAlbumData = await Album.findAll({ raw: true });
+    res.render("albums", {
+      dbAlbumData,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 
-router.get('/artist/:id', async (req, res) => {
-    try {
-      const dbArtistData = await Artist.findByPk(req.params.id);
-      const artistData = dbArtistData.get({ plain: true });
-      res.render('artist', { artistData });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  
+
+router.get("/artist/:id", async (req, res) => {
+  try {
+    const dbArtistData = await Artist.findByPk(req.params.id, {raw: true});
+    // const artistData = dbArtistData.get({ plain: true });
+    res.render("artist", { dbArtistData});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- models/Album.js
+++ models/Album.js
@@ -4,45 +4,109 @@ const sequelize = require("../config/connection");
 class Album extends Model {}
 
 Album.init(
-    {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  artist_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  artist_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    artist_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    artist_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
         model: "Artist",
         key: "id",
       },
+    },
+    producer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album_review: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    song_1: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_2: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_3: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_4: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_5: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_6: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_7: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_8: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_9: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_10: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_11: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_12: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_13: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_14: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    song_15: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
   },
-  producer_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  album_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  album_review: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "Album",
-  },
+  }
 );
 
-module.exports = Album
+module.exports = Album;
