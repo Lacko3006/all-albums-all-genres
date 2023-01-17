4bcaafe39c26f8d34ed85394bad06d4119ff3bcd
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-01-17 18:36:07 +0000

    fixed login links & added profile/loggedIn pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const Artist = require("../models/Artist");
 const Album = require("../models/Album");
+const User = require("../models/user");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
@@ -10,13 +11,29 @@ router.get("/login", async (req, res) => {
   res.render("login");
 });
 
+router.get("/loggedIn", async (req, res) => {
+  try {
+    const dbSignUpData = await User.findOne({
+      raw: true,
+      order: [["id", "DESC"]],
+    });
+    res.render("loggedIn", {
+      dbSignUpData,
+    });
+    console.log(dbSignUpData.name);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.get("/artist", async (req, res) => {
   try {
     const dbArtistData = await Artist.findAll({ raw: true });
     res.render("artist", {
       dbArtistData,
     });
-    console.log(dbArtistData)
+    console.log(dbArtistData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -39,23 +56,20 @@ router.get("/album", async (req, res) => {
 // GET album by PK
 router.get("/album/:id", async (req, res) => {
   try {
-    const dbAlbumData = await Album.findByPk(req.params.id, {raw: true});
+    const dbAlbumData = await Album.findByPk(req.params.id, { raw: true });
     // const artistData = dbArtistData.get({ plain: true });
-    res.render("review", { dbAlbumData});
+    res.render("review", { dbAlbumData });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-
-
-
 router.get("/artist/:id", async (req, res) => {
   try {
-    const dbArtistData = await Artist.findByPk(req.params.id, {raw: true});
+    const dbArtistData = await Artist.findByPk(req.params.id, { raw: true });
     // const artistData = dbArtistData.get({ plain: true });
-    res.render("artist", { dbArtistData});
+    res.render("artist", { dbArtistData });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
